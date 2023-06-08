9c7a72127ba0ee50942cbccf985b18cf0a29cb0a
Víctor Manuel Nuñez Sandra <122834431+VictorMNSalas@users.noreply.github.com>
2023-06-08 17:18:58 -0600

    new api

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
 const express = require("express");
+
 const app = express();
+app.set("port", process.env.PORT || 9000);
 
-app.use(express.json());
+const mysql = require("mysql");
+const myconn = require("express-myconnection");
+const dbOptions = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "students"
+};
 
-const students = [
-  { id: 1, name: "Jorge", age: 23, enroll: true },
-  { id: 2, name: "Mariana", age: 30, enroll: false },
-  { id: 3, name: "Antonio", age: 21, enroll: true },
-];
+//---------------------------------MIDDLEWARES---------------------------
+app.use(myconn(mysql, dbOptions, "single"));
 
+//---------------------------------ROUTES---------------------------
 app.get("/", (req, res) => {
-  res.send("Node JS api");
+  res.send("Welcome to my api");
 });
 
-app.get("/api/students", (req, res) => {
-  res.send(students);
+app.get("/api", (req, res) => {
+    res.send("API");
+  });
+
+//---------------------------------SERVER RUNING---------------------------
+app.listen(app.get("port"), () => {
+  console.log("Puerto ejecutandose...", app.get("port"));
 });
-
-app.get("/api/students/:id", (req, res) => {
-  const student = students.find((c) => c.id === parseInt(req.params.id));
-  if (!student) return res.status(404).send("Estudiante no encontrado");
-  else res.send(student);
-});
-
-
-app.post('/api/students', (req,res)=>{
-    const student = {
-        id: students.length + 1,
-        name: req.body.name,
-        age: req.body.age,
-        enroll: (req.body.enroll === 'true')
-    };
-
-    students.push(student);
-    res.send(student);
-});
-
-app.delete('/api/students/:id', (req,res)=>{
-    const student = students.find(e => e.id === parseInt(req.params.id));
-    if(!student) return res.status(404).send('Estudiantre no encontraado');
-    
-    const index = students.indexOf(student);
-    students.splice(index, 1);
-    res.send(student);
-});
-
-
-const port = process.env.port || 80
-app.listen(port, ()=> console.log(`Escuchando en el puerto ${port}`))
