438029dbbfcf4d2bc671d2d4d8cec951d53bcaec
Víctor Manuel Nuñez Sandra <122834431+VictorMNSalas@users.noreply.github.com>
2023-06-09 10:54:34 -0600

    API WITH MYSQL

--- api_routes.js
+++ api_routes.js
@@ -13,6 +13,18 @@ routes.get("/", (req, res) => {
   });
 });
 
+routes.get("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+
+    conn.query("SELECT * FROM students WHERE id = ?", [req.params.id],(err, rows) => {
+      if (err) return res.send(err);
+
+      res.json(rows);
+    });
+  });
+});
+
 routes.post("/", (req, res) => {
   req.getConnection((err, conn) => {
     if (err) return res.send(err);
@@ -25,4 +37,28 @@ routes.post("/", (req, res) => {
   });
 });
 
+routes.delete("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+
+    conn.query("DELETE FROM students WHERE id = ?",[req.params.id], (err, rows) => {
+      if (err) return res.send(err);
+
+      res.send('Student Deleted')
+    });
+  });
+});
+
+
+routes.put("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+
+    conn.query("UPDATE students SET ? WHERE id = ?",[req.body,req.params.id], (err, rows) => {
+      if (err) return res.send(err);
+
+      res.send('Student Updated')
+    });
+  });
+});
 module.exports = routes;
